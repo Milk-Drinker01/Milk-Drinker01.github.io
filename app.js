32d5a26899d6bd36b067bdbac5863970f5750bdc
Milk-Drinker01 <camerondus@gmail.com>
2022-04-29 18:46:58 -0400

    test parent script loading

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 function gohome() 
 {
-    document.getElementById("home").style.display = "block";
-    document.getElementById("milk-instancer").style.display = "none";
-    document.getElementById("contact-form").style.display = "none";
+    window.location.href = window.location.origin;
+    //document.getElementById("home").style.display = "block";
+    //document.getElementById("milk-instancer").style.display = "none";
+    //document.getElementById("contact-form").style.display = "none";
 }
 function loadContactForm() 
 {
