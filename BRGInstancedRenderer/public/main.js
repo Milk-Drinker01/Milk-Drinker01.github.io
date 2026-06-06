fa8f9d18130923a406fe7ea642cbeb9317a6a52c
Cameron Dus <camerondus@gmail.com>
2026-06-06 16:59:08 -0500

    remove analytics

--- BRGInstancedRenderer/public/main.js
+++ BRGInstancedRenderer/public/main.js
@@ -1,9 +1,1 @@
-// Inject GoatCounter analytics tag on every page.
-// Privacy-friendly, cookie-free, GDPR-compliant analytics.
-(function () {
-  var s = document.createElement('script');
-  s.src = 'https://gc.zgo.at/count.js';
-  s.async = true;
-  s.setAttribute('data-goatcounter', 'https://milk-drinker01.goatcounter.com/count');
-  document.head.appendChild(s);
-})();
+export default {}
